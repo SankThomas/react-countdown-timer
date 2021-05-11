d0136d3b5671198bc79d600796d329cb41ece6c4
Thomas Sankara <thomassankara74@gmail.com>
2021-05-11 16:29:53 +0300

    countdown component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react"
 
-function App() {
+const App = () => {
+  const [days, setDays] = useState(10)
+  const [hours, setHours] = useState(10)
+  const [minutes, setMinutes] = useState(10)
+  const [seconds, setSeconds] = useState(10)
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(() => {
+    const countdown = () => {
+      const endDate = new Date("December 25, 2021 00:00:00").getTime()
+      const today = new Date().getTime()
+
+      const timeDiff = endDate - today
+
+      const seconds = 1000
+      const minutes = seconds * 60
+      const hours = minutes * 60
+      const days = hours * 24
+
+      let timeDays = Math.floor(timeDiff / days)
+      let timeHours = Math.floor((timeDiff % days) / hours)
+      let timeMinutes = Math.floor((timeDiff % hours) / minutes)
+      let timeSeconds = Math.floor((timeDiff % minutes) / seconds)
+
+      timeHours = timeHours < 10 ? "0" + timeHours : timeHours
+      timeMinutes = timeMinutes < 10 ? "0" + timeMinutes : timeMinutes
+      timeSeconds = timeSeconds < 10 ? "0" + timeSeconds : timeSeconds
+
+      setDays(timeDays)
+      setHours(timeHours)
+      setMinutes(timeMinutes)
+      setSeconds(timeSeconds)
+      setIsLoading(false)
+    }
+
+    setInterval(countdown, 1000)
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      {isLoading ? (
+        <div className="loading">
+          <div className="spinner"></div>
+        </div>
+      ) : (
+        <section className="container">
+          <h1>React Countdown Timer</h1>
+
+          <div className="countdown">
+            <article>
+              <p>{days}</p>
+              <h3>Days</h3>
+            </article>
+            <article>
+              <p>{hours}</p>
+              <h3>Hours</h3>
+            </article>
+            <article>
+              <p>{minutes}</p>
+              <h3>Minutes</h3>
+            </article>
+            <article>
+              <p>{seconds}</p>
+              <h3>Seconds</h3>
+            </article>
+          </div>
+        </section>
+      )}
+    </>
+  )
 }
 
-export default App;
+export default App
